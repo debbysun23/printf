6c6dac9025d63a1c0027d168b3c08f9f781548a3
LovethEdeh <chidexdimma65@gmail.com>
2023-05-31 01:57:23 -0700

    printf project

--- printf.c
+++ printf.c
@@ -1,49 +1,55 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
-/**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
- */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+    va_list args;
+    int count = 0;
 
-	va_start(ap, format);
+    va_start(args, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
-	{
-		init_params(&params, ap);
-		if (*p != '%')
-		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
-	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++; // Move past the '%'
+
+            // Handle the conversion specifiers
+            switch (*format)
+            {
+                case 'c':
+                    // Character specifier
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    // String specifier
+                    count += printf("%s", va_arg(args, char *));
+                    break;
+                case '%':
+                    // Print a literal '%'
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    // Invalid specifier, print the character as is
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            // Regular character, print it
+            putchar(*format);
+            count++;
+        }
+
+        format++; // Move to the next character
+    }
+
+    va_end(args);
+
+    return count;
 }
